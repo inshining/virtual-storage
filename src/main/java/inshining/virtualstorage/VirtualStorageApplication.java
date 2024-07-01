6ffc87022609af11636a9528e26cc1521d9bba6a
inshining <inshining.kim@gmail.com>
2024-07-01 15:49:44 +0900

    inti: virtual storage project

---
+++ src/main/java/inshining/virtualstorage/VirtualStorageApplication.java
@@ -0,0 +1,13 @@
+package inshining.virtualstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/inshining/virtualstorage/VirtualStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package inshining.virtualstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
