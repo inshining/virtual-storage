dd04ebc25440c5d7b34a7700da2918f155e80bea
inshining <inshining.kim@gmail.com>
2024-07-04 15:46:29 +0900

    [FIX] : Test 실행 에러 Datasource 가 없어서 생긴 에러 였음 기존 mysql로 바라보게 되었기 때문에 runner위에서 돌아가지 않았음. 이 문제를 해결하기 위해서 h2 를 테스팅에서만 돌아가도록 만들었음.

--- src/test/java/inshining/virtualstorage/VirtualStorageApplicationTests.java
+++ src/test/java/inshining/virtualstorage/VirtualStorageApplicationTests.java
@@ -2,8 +2,10 @@ package inshining.virtualstorage;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class VirtualStorageApplicationTests {
 
     @Test
