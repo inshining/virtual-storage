8195cde97914e0135a8d7de395a11f20229527d5
inshining <inshining.kim@gmail.com>
2024-07-04 14:57:14 +0900

    [ADD] : 메타데이터 삭제 및 다운로드 관련 테스트 케이스 추가

--- src/test/java/inshining/virtualstorage/metadata/controller/MetaDataControllerTest.java
+++ src/test/java/inshining/virtualstorage/metadata/controller/MetaDataControllerTest.java
@@ -1,6 +1,7 @@
 package inshining.virtualstorage.metadata.controller;
 
 import inshining.virtualstorage.controller.MetaDataController;
+import inshining.virtualstorage.dto.FileDownloadDTO;
 import inshining.virtualstorage.dto.MetaDataFileResponse;
 import inshining.virtualstorage.service.MetaDataService;
 import org.junit.jupiter.api.Test;
@@ -11,7 +12,10 @@ import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayInputStream;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
@@ -81,5 +85,71 @@ public class MetaDataControllerTest {
                         .param("user", "test"))
                 .andExpect(status().isBadRequest())
                 .andExpect(content().string("Please select a file to upload"));
+    }
+
+    @Test
+    void testDeleteSuccess() throws Exception {
+        when(metaDataService.deleteFile("hello.txt", "test"))
+                .thenReturn(new MetaDataFileResponse(true, "File deleted successfully"));
+        mockMvc.perform(MockMvcRequestBuilders.delete("/file/")
+                        .param("file", "hello.txt")
+                        .param("user", "test"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("File deleted successfully"));
+    }
+
+    @Test
+    void testDeleteFailNotFound() throws Exception {
+        when(metaDataService.deleteFile("hello.txt", "test"))
+                .thenReturn(new MetaDataFileResponse(false, "File not found"));
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/file/")
+                        .param("file", "hello.txt")
+                        .param("user", "test"))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string("File not found"));
+    }
+    @Test
+    void testDeleteFailNoAuthorized() throws Exception {
+        when(metaDataService.deleteFile("hello.txt", "test"))
+                .thenReturn(new MetaDataFileResponse(false, "You are not authorized to delete this file"));
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/file/")
+                        .param("file", "hello.txt")
+                        .param("user", "test"))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string("You are not authorized to delete this file"));
+    }
+
+    @Test
+    void testDownloadSuccess() throws Exception{
+        when(metaDataService.downloadFile("hello.txt", "test"))
+                .thenReturn(new FileDownloadDTO(new ByteArrayInputStream("Hello, World!".getBytes()), "hello.txt", MediaType.TEXT_PLAIN, 13));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/file/download")
+                        .param("filename", "hello.txt")
+                        .param("user", "test"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_PLAIN))
+                .andExpect(content().string("Hello, World!"));
+    }
+
+    @Test
+    void testDownloadFailNoUsername() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/file/download")
+                        .param("filename", "hello.txt"))
+                .andExpect(status().isBadRequest());
+    }
+    @Test
+    void testDownloadFailNoFileName() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/file/download")
+                        .param("user", "test"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void testDownloadFailNoFileNameAndUsername() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/file/download"))
+                .andExpect(status().isBadRequest());
     }
 }
